195564f132b5bf10f36206411c7975036aaf8997
Bartłomiej kordek <bartekordek@gmail.com>
2022-04-27 20:40:21 +0200

    Update SDL2Wrapper. Move Components. Create Transform component.

---
+++ gameengine/include/gameengine/Components/TransformComponent.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "gameengine/Components/IComponent.hpp"
+
+#include "CUL/Math/Point.hpp"
+#include "CUL/Math/Rotation.hpp"
+#include "CUL/String.hpp"
+
+#include "CUL/STL_IMPORTS/STD_functional.hpp"
+#include "CUL/STL_IMPORTS/STD_map.hpp"
+
+#include "CUL/IMPORT_GLM.hpp"
+
+NAMESPACE_BEGIN( LOGLW )
+
+class IObject;
+
+using String = CUL::String;
+
+class GAME_ENGINE_API TransformComponent final: public IComponent
+{
+public:
+    using Pos = CUL::MATH::Point;
+    using TranslationVector = CUL::MATH::Point;
+
+
+    TransformComponent(IObject& owner);
+
+
+    void setWorldPosition( const Pos& position );
+    void setWorldPosition( Pos::Type x, Pos::Type y, Pos::Type z );
+    const Pos getWorldPosition() const;
+
+
+    void setWorldAngle( CUL::MATH::EulerAngles type, const CUL::MATH::Angle& angle );
+
+    void setWorldRotation(const CUL::MATH::Rotation& rotation);
+    const CUL::MATH::Rotation getWorldRotation() const;
+
+    const CUL::MATH::Angle& getWorldAngle( CUL::MATH::EulerAngles type ) const;
+    const glm::mat4 getModel();
+
+    const Pos& getPivot() const;
+    void setPivot( const Pos& pivot );
+
+    void addOnChangeCallback( const String& callbackName, const std::function<void( const Pos& position )> callback );
+    void removeCallback( const String& callbackName );
+
+    ~TransformComponent();
+protected:
+private:
+    IObject& m_owner;
+
+    Pos m_pos;
+    Pos m_pivot;
+
+    CUL::MATH::Rotation m_rotation;
+
+    std::map<String, std::function<void( const Pos& position )> > m_onChangeCallbacks;
+
+    // Deleted:
+    TransformComponent( const TransformComponent& value ) = delete;
+    TransformComponent( TransformComponent&& value ) = delete;
+    TransformComponent& operator=( const TransformComponent& value ) = delete;
+    TransformComponent& operator=( TransformComponent&& value ) = delete;
+};
+
+NAMESPACE_END( LOGLW )
