27ab5f2a0b1a1a62db0e0a84d41e283bbf2a3b78
Bartłomiej Kordek <bartekordek@gmail.com>
2023-05-06 01:28:41 +0200

    Add AttributeMeta.

---
+++ gameengine/include/gameengine/AttributeMeta.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "gameengine/BasicTypes.hpp"
+#include "CUL/String.hpp"
+#include "CUL/STL_IMPORTS/STD_vector.hpp"
+
+NAMESPACE_BEGIN( LOGLW )
+
+class VertexArray;
+
+struct AttributeMeta
+{
+    CUL::String Name;
+    int Index = 0;
+    int Size = 0;
+    DataType Type = DataType::NONE;
+    bool Normalized = false;
+    int Stride = 0;
+    void* Pointer = nullptr;
+
+    AttributeMeta( const CUL::String& inName, int inIndex, int inSize, DataType inType, bool inNormalized, int inStride, void* inPointer )
+        : Name( inName ),
+          Index( inIndex ),
+          Size( inSize ),
+          Type( inType ),
+          Normalized( inNormalized ),
+          Stride( inStride ),
+          Pointer( inPointer )
+    {
+    }
+
+    AttributeMeta() = delete;
+};
+
+struct VertexData
+{
+    LOGLW::PrimitiveType primitiveType = LOGLW::PrimitiveType::NONE;
+    unsigned VBO = 0u;
+    unsigned VAO = 0u;
+    std::vector<AttributeMeta> Attributes;
+    std::vector<float> vertices;
+    std::vector<unsigned> indices;
+};
+
+
+NAMESPACE_END( LOGLW )
