7d10bb698c49d3d0764e4ab27feb5e4833b5974e
Bartłomiej Kordek <bartekordek@gmail.com>
2024-12-28 01:18:01 +0100

    Add extension second possible extension string.

--- gameengine/src/Shaders/ShaderTypes.cpp
+++ gameengine/src/Shaders/ShaderTypes.cpp
@@ -5,12 +5,12 @@ using namespace LOGLW;
 
 CShaderTypes::ShaderType CShaderTypes::getShaderType( const CUL::String& extension )
 {
-    if( extension == ".vert" )
+    if( extension == ".vert" || extension == "vert" )
     {
         return ShaderType::Vertex;
     }
 
-    if( extension == ".frag" )
+    if( extension == ".frag" || extension == "frag" )
     {
         return ShaderType::Pixel;
     }
