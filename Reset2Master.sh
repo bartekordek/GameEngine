ba0c4103a124ad33d0fc550dbcc1544d23c1caf9
Bartłomiej Kordek <bartekordek@gmail.com>
2025-02-27 18:57:19 +0100

    Update Reset2Master.sh

--- Reset2Master.sh
+++ Reset2Master.sh
@@ -8,6 +8,9 @@ echo "Update helpers..."
 cd deps/helpers && ./Reset2Master.sh
 echo "Update helpers... done."
 
+
+cd ../..
+
 echo "Update CUL..."
 cd deps/CUL && ./Reset2Master.sh
 echo "Update CUL... done."
