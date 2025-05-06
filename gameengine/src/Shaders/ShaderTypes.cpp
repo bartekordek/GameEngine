fb53fc1524b676ba80f90c3479da18c2034fb407
Bartłomiej Kordek <bartekordek@gmail.com>
2025-05-06 18:20:54 +0200

    Add different shader extensions.

--- gameengine/src/Shaders/ShaderTypes.cpp
+++ gameengine/src/Shaders/ShaderTypes.cpp
@@ -5,12 +5,12 @@ using namespace LOGLW;
 
 CShaderTypes::ShaderType CShaderTypes::getShaderType( const CUL::String& extension )
 {
-    if( extension == ".vert" || extension == "vert" )
+    if( extension.equals( ".vs" ) || extension.equals( "vs" ) || extension == ".vert" || extension == "vert" )
     {
         return ShaderType::Vertex;
     }
 
-    if( extension == ".frag" || extension == "frag" )
+    if( extension.equals( ".fs" ) || extension.equals( "fs" ) || extension == ".frag" || extension == "frag" )
     {
         return ShaderType::Pixel;
     }
