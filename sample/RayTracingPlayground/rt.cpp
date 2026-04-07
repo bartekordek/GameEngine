0045c1f474c2a1b3d53a46989b65f5d218bb4e7b
Bartłomiej Kordek <bartekordek@gmail.com>
2026-04-07 16:32:18 +0200

    Draw sphere.

--- sample/RayTracingPlayground/rt.cpp
+++ sample/RayTracingPlayground/rt.cpp
@@ -71,6 +71,8 @@ void RT_Playground::run()
 
 void RT_Playground::afterInit()
 {
+    m_sphere.set_properties( Vec3f( 0.f, 0.f, -32.f ), 3.f, 128u );
+
     m_mainWindow = m_engine->getMainWindow();
     m_mainWindow->setBackgroundColor( LOGLW::ColorS( 1.0f, 0.0f, 0.0f, 1.0f ) );
     const auto& winSize = m_mainWindow->getSize();
@@ -96,23 +98,71 @@ void RT_Playground::afterInit()
         },
         40000 );
     auto fb = m_engine->getFrameBuffer();
-    fb->setSize( m_mainWindow->getSize().W * 0.5f, m_mainWindow->getSize().H );
+    fb->setSize( m_mainWindow->getSize().W, m_mainWindow->getSize().H );
 }
 
 void RT_Playground::timer()
 {
     const auto size = m_engine->getMainWindow()->getSize();
-
+    constexpr float screenZ = 0.f;  // screen plane at z = 0
     const std::size_t width = size.W;
     const std::size_t height = size.H;
+
+    static LOGLW::S_RGBA_F pix{0.f, 0.f, 0.f, 1.f};
+    static glm::vec3 eye{ 0.f, 0.f, 4.f };
 
     for( size_t j = 0; j < height; j++ )
     {
         for( size_t i = 0; i < width; i++ )
         {
-            const LOGLW::S_RGBA_F pix( (float)j / float( height ), (float)i / float( width ), 0.f, 1.f );
+            std::size_t type{ 1u };
+            if( type == 0u )
+            {
+                m_widget->updatePixel( i, j, pix );
+            }
+            else if( type == 1u )
+            {
+                float ndcX = ( i + 0.5f ) / width * 2.f - 1.f;
+                float ndcY = 1.f - ( j + 0.5f ) / height * 2.f;
 
-            m_widget->updatePixel( i, j, pix );
+                glm::vec3 pointOnScreen = glm::vec3( ndcX, ndcY, screenZ );
+                glm::vec3 rayDir = glm::normalize( pointOnScreen - eye );
+
+                glm::vec3 intersectionPoint;
+                glm::vec3 intersectionNormal;
+                if( glm::intersectRaySphere( eye,                   // ray origin
+                                             rayDir,                // normalized direction
+                                             m_sphere.getCenter(),  // sphere center
+                                             m_sphere.getRadius(),  // radius
+                                             intersectionPoint,     // output hit position
+                                             intersectionNormal     // output normal
+                                             ) )
+                {
+                    pix.Red = 1.f;
+                    pix.Green = 0.f;
+                    pix.Blue = 0.f;
+                }
+                else
+                {
+                    pix.Red = 0.f;
+                    pix.Green = 0.f;
+                    pix.Blue = 0.f;
+                }
+
+                m_widget->updatePixel( i, j, pix );
+            }
+            else
+            {
+                Vec3f dir = Vec3f( i, j, -1 );
+                dir = glm::normalize( dir );
+
+                const Vec3f ray = cast_ray( Vec3f( 0, 0, 0 ), dir, m_sphere );
+
+                pix.Red = j / float( height );
+                pix.Green = i / float( width );
+                pix.Blue = 0.f;
+                m_widget->updatePixel( i, j, pix );
+            }
         }
     }
 
@@ -165,6 +215,27 @@ void RT_Playground::onKeyBoardEvent( const LOGLW::KeyboardState& key )
     {
         closeApp();
     }
+
+    if( key.at( "W" ) )
+    {
+        m_sphere.move( Vec3f( 0.f, 0.f, -1.0f ) );
+        m_engine->getLoger()->logInfo( "Sphere pos: %f, %f, %f", m_sphere.getCenter().x, m_sphere.getCenter().y, m_sphere.getCenter().z );
+    }
+    else if( key.at( "S" ) )
+    {
+        m_sphere.move( Vec3f( 0.f, 0.f, 1.0f ) );
+        m_engine->getLoger()->logInfo( "Sphere pos: %f, %f, %f", m_sphere.getCenter().x, m_sphere.getCenter().y, m_sphere.getCenter().z );
+    }
+    else if( key.at( "U" ) )
+    {
+        m_sphere.resize( 2.f );
+        m_engine->getLoger()->logInfo( "Sphere radius: %f", m_sphere.getRadius() );
+    }
+    else if( key.at( "J" ) )
+    {
+        m_sphere.resize( 0.5f );
+        m_engine->getLoger()->logInfo( "Sphere radius: %f", m_sphere.getRadius() );
+    }
 }
 
 void RT_Playground::onWindowEvent( const LOGLW::WindowEvent::Type type )
@@ -181,4 +252,61 @@ void RT_Playground::closeApp()
 
     m_engine->stopRenderingLoop();
     m_engine->stopEventLoop();
+}
+
+void Sphere::set_properties( const Vec3f& c, const float& r, std::size_t pointsCount )
+{
+    m_center = c;
+    m_radius = r;
+    m_pointsCount = pointsCount;
+}
+
+std::size_t Sphere::getPointsCount() const
+{
+    return m_pointsCount;
+}
+
+glm::vec3 Sphere::getPoint( std::size_t i ) const
+{
+    const float goldenAngle = glm::pi<float>() * ( 3.0f - std::sqrt( 5.0f ) );
+
+    float t = static_cast<float>( i ) / static_cast<float>( m_pointsCount );
+
+    float y = 1.0f - 2.0f * t;
+    float r = std::sqrt( 1.0f - y * y );
+
+    float theta = goldenAngle * static_cast<float>( i );
+
+    float x = r * std::cos( theta );
+    float z = r * std::sin( theta );
+
+    return m_center + m_radius * glm::vec3( x, y, z );
+}
+
+bool Sphere::ray_intersect( const Vec3f& orig, const Vec3f& dir, float& t0 ) const
+{
+    Vec3f L = m_center - orig;
+    float tca = glm::length( L * dir );
+    float d2 = glm::length( L * L ) - tca * tca;
+    if( d2 > m_radius * m_radius )
+        return false;
+    float thc = sqrtf( m_radius * m_radius - d2 );
+    t0 = tca - thc;
+    float t1 = tca + thc;
+    if( t0 < 0 )
+        t0 = t1;
+    if( t0 < 0 )
+        return false;
+    return true;
+}
+
+
+Vec3f cast_ray( const Vec3f& orig, const Vec3f& dir, const Sphere& sphere )
+{
+    float sphere_dist = std::numeric_limits<float>::max();
+    if( !sphere.ray_intersect( orig, dir, sphere_dist ) )
+    {
+        return Vec3f( 0.2, 0.7, 0.8 );  // background color
+    }
+    return Vec3f( 0.4, 0.4, 0.3 );
 }
--- sample/RayTracingPlayground/rt.hpp
+++ sample/RayTracingPlayground/rt.hpp
@@ -33,6 +33,46 @@ enum class Type : short;
 }
 }  // namespace LOGLW
 
+using Vec3f = glm::vec3;
+
+
+
+class Sphere
+{
+public:
+    void set_properties( const Vec3f& c, const float& r, std::size_t pointsCount );
+    bool ray_intersect( const Vec3f& orig, const Vec3f& dir, float& t0 ) const;
+    std::size_t getPointsCount() const;
+    glm::vec3 getPoint( std::size_t i ) const;
+    const Vec3f& getCenter() const
+    {
+        return m_center;
+    }
+
+    void move( const Vec3f& delta )
+    {
+        m_center += delta;
+    }
+
+    void resize( float delta )
+    {
+        m_radius *= delta;
+    }
+
+    float getRadius() const
+    {
+        return m_radius;
+    }
+
+protected:
+private:
+    Vec3f m_center;
+    float m_radius;
+    std::size_t m_pointsCount;
+};
+
+Vec3f cast_ray( const Vec3f& orig, const Vec3f& dir, const Sphere& sphere );
+
 class RT_Playground final: public LOGLW::IMouseObserver, public LOGLW::IKeyboardObserver
 {
 public:
@@ -58,6 +98,7 @@ private:
     LOGLW::IWindow* m_mainWindow = nullptr;
     LOGLW::Camera* m_camera = nullptr;
 
+    Sphere m_sphere;
 
     glm::vec3 g_eyePos;
 
