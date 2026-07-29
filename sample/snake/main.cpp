48ade594c6ac0cf62802e4c7196ed5ac7552b631
Bartłomiej Kordek <bartekordek@gmail.com>
2026-07-29 18:09:13 +0200

    Build fix.

--- sample/snake/main.cpp
+++ sample/snake/main.cpp
@@ -1,19 +1,20 @@
-#include "Game.hpp"
 #include "CUL/GenericUtils/ConsoleUtilities.hpp"
+#include "Game.hpp"
 
 int main( int argc, char** argv )
 {
     auto& cu = CUL::GUTILS::ConsoleUtilities::getInstance();
     cu.setArgs( argc, argv );
 
-    auto width =
-        cu.getFlagValue( "-w" ) ? cu.getFlagValue( "-w" ).value() : CUL_STR( "1280" );
-    auto height =
-        cu.getFlagValue( "-h" ) ? cu.getFlagValue( "-h" ).value() : CUL_STR( "800" );
+    auto width = cu.getDoesFlagExist( "-w" ) ? cu.getFlagValue( "-w" )
+                                             : CUL::String( CUL_STR( "1280" ) );
+    auto height = cu.getDoesFlagExist( "-h" ) ? cu.getFlagValue( "-h" )
+                                              : CUL::String( CUL_STR( "800" ) );
 
     std::int32_t size = 10;
-    LOGLW::WinPos winPos{.X = 64u, .Y = 64u};
-    LOGLW::WinSize winSize{ .W = static_cast<std::uint16_t>(width.toInt()), .H = static_cast<std::uint16_t>(height.toInt()) };
+    LOGLW::WinPos winPos{ .X = 64u, .Y = 64u };
+    LOGLW::WinSize winSize{ .W = static_cast<std::uint16_t>( width.toInt() ),
+                            .H = static_cast<std::uint16_t>( height.toInt() ) };
     Game snakeGame( size, size, winPos, winSize );
     snakeGame.run();
 
--- tools/shader_editor/main.cpp
+++ tools/shader_editor/main.cpp
@@ -1,19 +1,16 @@
-#include "ShaderEditor.hpp"
-
 #include "CUL/GenericUtils/ConsoleUtilities.hpp"
+#include "ShaderEditor.hpp"
 
 int main( int argc, char** argv )
 {
     auto& cu = CUL::GUTILS::ConsoleUtilities::getInstance();
     cu.setArgs( argc, argv );
 
-    auto width =
-        cu.getFlagValue( "-w" ) ? cu.getFlagValue( "-w" ).value() : CUL_STR( "1280" );
-    auto height =
-        cu.getFlagValue( "-h" ) ? cu.getFlagValue( "-h" ).value() : CUL_STR( "800" );
+    auto width = cu.getFlagValue( "-w", CUL_STR( "1280" ) );
+    auto height = cu.getFlagValue( "-h", CUL_STR( "800" ) );
 
-    auto x = cu.getFlagValue( "-x" ) ? cu.getFlagValue( "-x" ).value() : CUL_STR( "256" );
-    auto y = cu.getFlagValue( "-y" ) ? cu.getFlagValue( "-y" ).value() : CUL_STR( "256" );
+    auto x = cu.getFlagValue( "-X", CUL_STR( "128" ) );
+    auto y = cu.getFlagValue( "-y", CUL_STR( "128" ) );
 
     LOGLW::WinData wd;
     wd.WindowRes.W = static_cast<std::uint16_t>( width.toInt() );
